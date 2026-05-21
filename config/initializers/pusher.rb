26d301a607f083dbe70ee081b52371e1bfe9249e
bishesh-ganesh-shrestha <unikbisheshshrestha12@gmail.com>
2026-05-22 01:05:27 +0545

    add pusher initializer

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,19 @@
+require "pusher"
+
+Pusher.app_id =
+  Rails.application.credentials
+       .pusher[:app_id]
+
+Pusher.key =
+  Rails.application.credentials
+       .pusher[:key]
+
+Pusher.secret =
+  Rails.application.credentials
+       .pusher[:secret]
+
+Pusher.cluster =
+  Rails.application.credentials
+       .pusher[:cluster]
+
+Pusher.encrypted = true
